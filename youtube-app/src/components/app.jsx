f1c2e15a6fbdb85b7bb1615a07522bae1776f652
Giancarlo Coca <giancarlococa@Giancarlos-MacBook-Pro.local>
2021-07-12 17:31:32 -0700

    need navbar return statements

--- youtube-app/src/components/app.jsx
+++ youtube-app/src/components/app.jsx
@@ -1,9 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
+import NavBar from '../components/navbar/NavBar';
+
 const AppHooks = () => {
-videoId
-comment
+
+return(
+    <>
+        <NavBar/>
+    </>
+)
 }
 
 export default AppHooks;
