aa880d4f525b5da299912c25964ddc6bc2860217
Giancarlo Coca <giancarlococa@Giancarlos-MacBook-Pro.local>
2021-07-12 19:21:33 -0700

    added onSubmit event, need to implement the function

--- youtube-app/src/components/app.jsx
+++ youtube-app/src/components/app.jsx
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-
+import apiKey from '../../keys';
 import NavBar from './navbar/NavBar';
 import VideoPlayer from './videoPlayer/VideoPlayer';
 
 const AppHooks = () => {
 
+    
 return(
     <>
         <NavBar/>
--- youtube-app/src/components/navbar/Navbar.jsx
+++ youtube-app/src/components/navbar/Navbar.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import './NavBar.css';
+import apiKey from '../../keys';
+
+const  NavBar = (props) => {
+
+const [collections, setCollection] = useState({});
+
+//get useEffect() from props to be handle by the onChange and onSubmit events here
+useEffect((userInput) => {
+    axios.get(`https://www.googleapis.com/youtube/v3/search?q=${userInput}&key=${apiKey}`)
+})
+
+function onChange(event){
+    console.log(event)
+    return userInput 
+}
+
+function onSubmit(){
+    let userInput = event.something;
+    useEffect(userInput)
+}
 
 
-function  NavBar () {
+
     return (
         <div>
             <nav className ="navbar navbar-light bg-light justify-content-between">
-                <a className="navbar-brand">Navbar</a>
+                <a className="navbar-brand">Youtube</a>
                 <form className="form-inline">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    <input className="form-control mr-sm-2" 
+                    type="search" 
+                    placeholder="Search" 
+                    aria-label="Search"
+                    >
+                    
+                    </input>
+                    <button className="btn btn-outline-success my-2 my-sm-0" 
+                    type="submit"
+                    onSubmit={onSubmit}>Search</button>
                 </form>
             </nav>
         </div>
