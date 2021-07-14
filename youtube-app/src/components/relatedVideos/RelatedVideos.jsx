4867715ed0f8cfb9314c23c03b552d33edf2959c
Giancarlo Coca <giancarlococa@Giancarlos-MacBook-Pro.local>
2021-07-14 16:07:24 -0700

    retrieved thumbnail image from video object

--- youtube-app/src/components/relatedVideos/RelatedVideos.jsx
+++ youtube-app/src/components/relatedVideos/RelatedVideos.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 import './RelatedVideos.css';
 //testg
 const relatedVideos = (props) => {
-    console.log("relatedVideos",props);
-
+    //console.log("relatedVideos",props);
+    //console.log("URL",props.videoObject.items[1].snippet.thumbnails.default.url)
     
     return(
-        
-        <div class="card-container" style={{width:'18rem'}}>
-            <img></img>
-        <div class="card-body">{props.videoObject.items[1].snippet.thumbnails.default.url}</div>
-        <div class="card-body"> </div>
-        <div class="card-body"> </div>
-        <div class="card-body"> </div>
-</div>
-    )
+        <>
+            {props.videoObject.items ? 
+            <div class="card-container" style={{width:'18rem'}}>
+                
+            <div  class="card-body"> 
+                <img src ="https://i.ytimg.com/vi/R3Ple1xplhs/default.jpg"></img>
+            </div>
+            <div class="card-body"> </div>
+            <div class="card-body"> </div>
+            <div class="card-body"> </div>
+            </div>
+            : <h1></h1>
+            }
+
+        </>
+    );
 }
 
 export default relatedVideos;
