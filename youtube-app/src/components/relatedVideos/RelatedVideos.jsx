bf16b551cbc2ac26dc73303df126dbac29e0866d
JordanSchwartz97 <schwartzj1297@gmail.com>
2021-07-14 18:07:58 -0500

    Staged changes

--- youtube-app/src/components/relatedVideos/RelatedVideos.jsx
+++ youtube-app/src/components/relatedVideos/RelatedVideos.jsx
@@ -3,13 +3,13 @@ import './RelatedVideos.css';
 //testg
 const relatedVideos = (props) => {
     console.log("relatedVideos",props);
-
+    
     
     return(
         
         <div class="card-container" style={{width:'18rem'}}>
-            <img></img>
-        <div class="card-body">{props.videoObject.items[1].snippet.thumbnails.default.url}</div>
+            <img src ="https://i.ytimg.com/vi/R3Ple1xplhs/default.jpg"></img>
+        <div class="card-body"></div>
         <div class="card-body"> </div>
         <div class="card-body"> </div>
         <div class="card-body"> </div>
