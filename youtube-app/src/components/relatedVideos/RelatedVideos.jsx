75cf18937f50c4a1b35f116f8991123e456efa5d
JordanSchwartz97 <schwartzj1297@gmail.com>
2021-07-14 18:08:53 -0500

    staged changes

--- youtube-app/src/components/relatedVideos/RelatedVideos.jsx
+++ youtube-app/src/components/relatedVideos/RelatedVideos.jsx
@@ -2,21 +2,6 @@ import React from 'react';
 import './RelatedVideos.css';
 //testg
 const relatedVideos = (props) => {
-<<<<<<< HEAD
-    console.log("relatedVideos",props);
-    
-    
-    return(
-        
-        <div class="card-container" style={{width:'18rem'}}>
-            <img src ="https://i.ytimg.com/vi/R3Ple1xplhs/default.jpg"></img>
-        <div class="card-body"></div>
-        <div class="card-body"> </div>
-        <div class="card-body"> </div>
-        <div class="card-body"> </div>
-</div>
-    )
-=======
     //console.log("relatedVideos",props);
     //console.log("URL",props.videoObject.items[1].snippet.thumbnails.default.url)
     
@@ -37,7 +22,6 @@ const relatedVideos = (props) => {
 
         </>
     );
->>>>>>> 4867715ed0f8cfb9314c23c03b552d33edf2959c
 }
 
 export default relatedVideos;
