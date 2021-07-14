5bf5f7013b08766b889b91c6a7d540e51e2f439f
JordanSchwartz97 <schwartzj1297@gmail.com>
2021-07-14 17:48:04 -0500

    Added related video props

--- youtube-app/src/components/relatedVideos/RelatedVideos.jsx
+++ youtube-app/src/components/relatedVideos/RelatedVideos.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './RelatedVideos.css';
 //testg
-const relatedVideos = () => {
+const relatedVideos = (props) => {
+    console.log("relatedVideos",props);
+
     
-
-
     return(
+        
         <div class="card-container" style={{width:'18rem'}}>
             <img></img>
-        <div class="card-body"> </div>
+        <div class="card-body">{props.videoObject.items[1].snippet.thumbnails.default.url}</div>
         <div class="card-body"> </div>
         <div class="card-body"> </div>
         <div class="card-body"> </div>
