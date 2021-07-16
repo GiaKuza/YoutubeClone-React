8b367cffb47e854d1c47d08af008b76c02fbff7e
JordanSchwartz97 <schwartzj1297@gmail.com>
2021-07-15 19:31:25 -0500

    staged changes

--- youtube-app/src/components/videoDescription/VideoDescription.jsx
+++ youtube-app/src/components/videoDescription/VideoDescription.jsx
@@ -3,14 +3,16 @@ import './VideoDescription.css';
 
 //this will be pulled from youtube api and accessed with props. Just add div for container for description/title
 const VideoDescription= (props) => {
-
     //add cond"itional rendering 
   console.log("videoTitle:",props.videoTitle)
   console.log("videoDescription", props.videoDescription)
+
     return(
-        <div>
-            <h5></h5>
+        <div className='video-info'>
+        <h5 className="video-title"> video title</h5>
+        <h5 className="video-description">video Description</h5>
         </div>
+        
     );
 }
 
